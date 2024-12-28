c397fba03fd6cfe359c885f20ac2aa0b1c78ee50
zhuchangjun <zhuchangjun@hotmail.com>
2024-12-28 18:02:29 -0500

    changes for cors

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
-//app.use(cors())
+app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 // db connection
 connectDB()
